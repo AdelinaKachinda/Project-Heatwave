6e82fb01aa4e8aae339db329bfc0fd55e4a2e38f
Marysabel Morales <marysabel.m31@gmail.com>
2022-07-20 02:22:06 +0000

    Attempting to pull

--- app.py
+++ app.py
@@ -10,9 +10,5 @@ def index():
     return render_template('index.html')
 
 
-<<<<<<< HEAD
-if __name__ == '__main__':      
-=======
-if __name__ == "__main__":
->>>>>>> 6edff745bd481705f7c2d7a5a00776139b5c6893
+if __name__ == "__main__":      
     app.run(debug=True, host="0.0.0.0")
